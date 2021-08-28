77dcebae84152134e4aeab2bd1604bc9d0c3679c
Sophy Sun <sophyzy.sun@mail.utoronto.ca>
2021-08-28 13:32:45 -0400

    setting up request api

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.db.models.fields.related import OneToOneField
 
 # Create your models here.
 
 def request_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT / request_<id>/<filename>
-    return 'request_{0}/{1}'.format(instance.id, filename)
+    return 'request_{0}/{1}'.format(instance.request_id, filename)
 
 class Contacts(models.Model):
     contact_id = models.BigAutoField(primary_key=True)
@@ -22,13 +23,19 @@ class Contacts(models.Model):
 
 
 class Requests(models.Model):
+    BUSINESS_TYPE = (
+        ('R', 'Restaurant'),
+        ('F', 'Farm'),
+    )
     request_id = models.BigAutoField(primary_key=True)
     request_text = models.CharField(max_length=500) 
     post_date = models.DateTimeField('date posted')
     request_photo = models.ImageField(upload_to = request_directory_path)
     contact_id = models.ForeignKey(
-        Contacts, on_delete=models.CASCADE,
+        Contacts, 
+        on_delete=models.CASCADE,
     )
+    business_type = models.CharField(max_length=1, choices=BUSINESS_TYPE, default='R')
     fullfilled = models.BooleanField(default=False)
 
 
--- connectlocal/urls.py
+++ connectlocal/urls.py
@@ -13,10 +13,48 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from backend.models import Requests
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.urls import include, path
+from rest_framework import routers, serializers, viewsets, permissions
+from django_filters import rest_framework as filters
+from rest_framework import generics
+
+# Serializers define the API representation.
+class RequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Requests
+        fields = '__all__'
+
+BUSINESS_CHOICES = (
+        ('R', 'Restaurant'),
+        ('F', 'Farm'),
+)
+
+class RequestFilter(filters.FilterSet):
+    business_type = filters.ChoiceFilter(choices=BUSINESS_CHOICES)
+    class Meta:
+        model = Requests
+        fields = ['business_type']
+
+class RequestViewSet(viewsets.ModelViewSet):
+   
+    queryset = Requests.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = RequestSerializer
+    filter_class = RequestFilter
+    filterset_fields = ('business_type')
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register('requests', RequestViewSet, 'requests')
 
 urlpatterns = [
     path('', include('backend.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
