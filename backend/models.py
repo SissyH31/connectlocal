d4b9bfcec44153111d6413c1d2d7646b8906340e
Sophy Sun <sophyzy.sun@mail.utoronto.ca>
2021-08-28 10:56:25 -0400

    added models for Requests

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+def request_directory_path(instance, filename):
+  
+    # file will be uploaded to MEDIA_ROOT / request_<id>/<filename>
+    return 'request_{0}/{1}'.format(instance.id, filename)
+
 class Contacts(models.Model):
     contact_id = models.BigAutoField(primary_key=True)
     business_name = models.CharField( max_length=50)
@@ -13,10 +19,17 @@ class Contacts(models.Model):
     state = models.CharField(max_length=30)
     zip = models.CharField(max_length=20)
     country = models.CharField(max_length=25)
+    business_type = models.CharField(max_length=1, choices=BUSINESS_TYPE)
 
 
 class Requests(models.Model):
     request_id = models.BigAutoField(primary_key=True)
+    request_text = models.CharField(max_length=500)   # includes address & contact info
+    post_date = models.DateTimeField('date posted')
+    request_photo = models.ImageField(upload_to = request_directory_path)
+    contact_id = models.ForeignKey(
+        Contacts, on_delete=models.CASCADE,
+    )
 
 
 class Orders(models.Model):
